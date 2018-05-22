a4b53175653036043539ee5f3c777c05011c9d93
Евгений <eugene@localhost.localdomain>
2018-05-22 19:11:37 +0700

    Fixed

--- src/main.c
+++ src/main.c
@@ -10,23 +10,23 @@ int main(int argc, char *argv[])
     int position;
 
     if (argc != 3) {
-		printf("Параметры заданы неверно\n");
-		return 1;
+	printf("Invalid argument\n");
+	return 1;
     }
 
     in = fopen(argv[2], "r");
     if (in == NULL) {
-		printf("Ошибка открытия файла\n");
-		return 1;
+	printf("Error open file\n");
+	return 1;
     }
 
     while (!feof(in)) {
-		fgets(str, 10000, in);
-		position = kmp(str, argv[1]);
-		if (position > 0){
-			printf("%d: %d\n", counter, position + 1);
-		}
-		counter++;
+	fgets(str, 10000, in);
+	position = kmp(str, argv[1]);
+	if (position > 0){
+	    printf("%d: %d\n", counter, position + 1);
+	}
+	counter++;
     }
     fclose(in);
 
