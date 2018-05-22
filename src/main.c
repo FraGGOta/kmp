9b2215077964375dc483db7ac29d7dae4f017651
Евгений <eugene@localhost.localdomain>
2018-05-22 17:50:56 +0700

    Fixed

---
+++ src/main.c
@@ -0,0 +1,34 @@
+#include "kmp.h"
+#include "kmp.c"
+
+int main(int argc, char *argv[])
+{
+    FILE *in;
+    int N = 10000;
+    char str[N];
+    int counter = 1;
+    int position;
+
+    if (argc != 3) {
+	printf("Параметры заданы неверно\n");
+	return 1;
+    }
+
+    in = fopen(argv[2], "r");
+    if (in == NULL) {
+	printf("Ошибка открытия файла\n");
+	return 1;
+    }
+
+    while (!feof(in)) {
+    fgets(str, 10000, in);
+    position = kmp(str, argv[1]);
+    if (position > 0){
+        printf("%d: %d\n", counter, position + 1);
+		}
+		counter++;
+    }
+    fclose(in);
+
+    return 0;
+}
